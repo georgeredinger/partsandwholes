236a671ecce120661352bc442d3ece02c6debb83
George Redinger <george@georgeredinger.com>
2012-08-22 14:45:15 -0700

    database push

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysqldump --add-drop-table  -u partsandwholes -p$REDINGERDRESSAGE_MYSQL_PASSWORD partsandwholes > partsandwhohles
+mysqldump --add-drop-table  -u george -p$GEORGEREDINGER_MYSQL_PASSWORD partsandwholes > partsandwholes
 sed "s/http:\/\/partsandwholes/http:\/\/gr.redinger.me/g" partsandwholes > gr.redinger.me.sql
